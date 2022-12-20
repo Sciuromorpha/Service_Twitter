79fe52338ff94ce471f1d57e11e2ea37fa1277b1
Whale Mo <ncwhale@gmail.com>
2022-12-20 16:20:29 +0900

    Rename config export module names.

--- src/sciuromorpha_service_twitter/config.py
+++ src/sciuromorpha_service_twitter/config.py
@@ -2,8 +2,8 @@ from . import static as S
 from sciuromorpha_core.config import config as core_config
 from nameko.constants import AMQP_URI_CONFIG_KEY
 
-config = {
+amqp_config = {
     AMQP_URI_CONFIG_KEY: core_config[S.CONFIG_SECTION_MESSAGEQUEUE]["url"]
 }
 
-twitter = core_config[S.CONFIG_SECTION_TWITTER]
+twitter_config = core_config[S.CONFIG_SECTION_TWITTER]
